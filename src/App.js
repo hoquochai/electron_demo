b7c06f323296e185b8021425bd9c81e4365814fc
haihq1 <haiho@novahub.vn>
2022-07-11 08:02:20 +0700

    login form

--- src/App.js
+++ src/App.js
@@ -2,18 +2,36 @@ import './App.css';
 import Create from './components/user/create';
 import Read from './components/user/index';
 import Update from './components/user/update';
-import { BrowserRouter as Router,Route, Routes } from 'react-router-dom'
+import Header from './components/Header/Header';
+import RegistrationForm from './components/RegistrationForm/RegistrationForm';
+import { 
+    BrowserRouter as Router,
+    Route,
+    Routes
+} from "react-router-dom";
 
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path='/create' element={<Create/>}/>
-                <Route path='/' element={<Read/>}/>
-                <Route path='/update' element={<Update/>}/>
-            </Routes>
-        </Router>
-    );
+        <div className='App'>
+            <Header />
+            <div className="container d-flex align-items-center flex-column">
+                <Router>
+                    <Routes>
+                        <Route path='/' element={<RegistrationForm />} />
+                    </Routes>
+                </Router>
+            </div>
+        </div>
+    )
+    // return (
+    //     <Router>
+    //         <Routes>
+    //             <Route path='/create' element={<Create/>}/>
+    //             <Route path='/' element={<Read/>}/>
+    //             <Route path='/update' element={<Update/>}/>
+    //         </Routes>
+    //     </Router>
+    // );
 }
 
 export default App;
